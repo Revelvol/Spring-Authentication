c71331f131932fb12332524d3f0956295e740607
Revelvol <julius.sutandi@gmail.com>
2023-05-14 20:45:08 +0700

    implemented jwt filter, making jwt service to extract data from jwt

--- src/main/java/com/revelvol/JWT/config/JwtAuthenticationFilter.java
+++ src/main/java/com/revelvol/JWT/config/JwtAuthenticationFilter.java
@@ -1,4 +1,45 @@
 package com.revelvol.JWT.config;
 
-public class JwtAuthenticationFilter {
+import com.revelvol.JWT.service.JwtService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtService jwtService;
+    @Autowired
+    public JwtAuthenticationFilter(JwtService jwtService) {
+        this.jwtService = jwtService;
+    }
+
+    @Override
+    protected void doFilterInternal(
+            @NonNull HttpServletRequest request,
+            @NonNull HttpServletResponse response,
+            @NonNull FilterChain filterChain //do filter in the chain
+    ) throws ServletException, IOException {
+        //get the authorization where the jwt token reside
+        final String authHeader = request.getHeader("Authorization");
+        final String jwt;
+        final String userEmail;
+        // do a check if there is auth header  or not start with bearer
+        if (authHeader == null || !authHeader.startsWith("Bearer")) {
+            // pass to another filter (yang hanle return )
+            filterChain.doFilter(request,response);
+            return;
+        }
+        // extract the string after the bearer prefix
+        jwt = authHeader.substring(7);
+        userEmail = jwtService.extractUsername(jwt); // todo extract the user email from the jwt token
+
+    }
 }
---
+++ src/main/java/com/revelvol/JWT/service/JwtService.java
@@ -0,0 +1,10 @@
+package com.revelvol.JWT.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtService {
+    public String extractUsername(String jwt) {
+        return "helo";
+    }
+}
