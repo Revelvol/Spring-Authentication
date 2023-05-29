25a2661e9e4834840f8dc775e20d060ebff7159d
Revelvol <julius.sutandi@gmail.com>
2023-05-29 23:44:15 +0700

    configured bean in the appconfig for authentication manager

---
+++ src/main/java/com/revelvol/JWT/auth/AuthenticationController.java
@@ -0,0 +1,6 @@
+package com.revelvol.JWT.auth;
+
+//rest api to allow creation and uncreation of stuff
+
+public class AuthenticationController {
+}
--- src/main/java/com/revelvol/JWT/config/ApplicationConfig.java
+++ src/main/java/com/revelvol/JWT/config/ApplicationConfig.java
@@ -3,8 +3,14 @@ package com.revelvol.JWT.config;
 import com.revelvol.JWT.repository.UserRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 //hold configuration like bean etc
 @Configuration
@@ -21,4 +27,26 @@ public class ApplicationConfig {
         return username -> repository.findByEmail(username)
                 .orElseThrow(() -> new UsernameNotFoundException("User not Found"));
     }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        // set which user detail you want to use (because you might want to use in memory etc)
+        authenticationProvider.setUserDetailsService(userDetailsService());
+        authenticationProvider.setPasswordEncoder(passwordEncoder());
+
+        return authenticationProvider;
+    }
+
+    @Bean
+    private PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean//authentication manager is a class that let you manage authentication as a whole
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+
+
 }
