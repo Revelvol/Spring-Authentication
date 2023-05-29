ba9dd3f2f1d57f6a58e50b7c99f1255a4c3b537f
Revelvol <julius.sutandi@gmail.com>
2023-05-29 23:16:35 +0700

    configuring app and security config

---
+++ src/main/java/com/revelvol/JWT/config/SecurityConfig.java
@@ -0,0 +1,46 @@
+package com.revelvol.JWT.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+// we want to add/ implement the jwt authentication filter to the normal java security filter chain
+public class SecurityConfig {
+
+    private final JwtAuthenticationFilter jwtAuthFilter;
+    private final AuthenticationProvider authenticationProvider;
+
+    public SecurityConfig(JwtAuthenticationFilter jwtAuthFilter, AuthenticationProvider authenticationProvider) {
+        this.jwtAuthFilter = jwtAuthFilter;
+        this.authenticationProvider = authenticationProvider;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+
+        http
+                .csrf()
+                .disable()
+                // endpoint whitelist that do not need authentication
+                .authorizeHttpRequests()
+                .requestMatchers("") //all the list requestin here will permit all
+                .permitAll()
+                .anyRequest()// any other request need to be authenticated
+                .authenticated()
+                .and() // add new configuration
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS) //add new session for each request
+                .and()
+                .authenticationProvider(authenticationProvider)
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class); // we want jwt filter to do first before the password
+                
+        return http.build();
+    }
+}
